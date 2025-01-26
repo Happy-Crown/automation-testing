3e4e0822bdeb21c93fb1fce4e87dd9bbca4c5a8a
Dmitry <polnikoff.d@ya.ru>
2025-01-26 22:04:44 +0300

    Create DDT parametrized tests

---
+++ homework/ddt_tests.py
@@ -0,0 +1,31 @@
+import pytest
+import requests
+from api_tests import base_url
+
+@pytest.mark.parametrize("post_id, status_code",[
+    (1, 200),
+    (15, 200),
+    (552525, 404)
+])
+def test_parametrize_get_requests(base_url, post_id, status_code):
+    response = requests.get(f"{base_url}/{post_id}")
+    assert response.status_code == status_code
+
+@pytest.mark.parametrize("test_data",[
+    {"userId": 11, "title": "test 11", "body": "test 11. test 11"},
+    {"userId": 12, "title": "test 12", "body": "test 12. test 12"}
+])
+def test_parametrize_post_requests(base_url, test_data):
+    response = requests.post(base_url, test_data)
+    assert response.status_code == 201
+    for key in test_data:
+        assert response.json()[key] == test_data[key]
+
+@pytest.mark.parametrize("post_id, status_code", [
+    (1, 200),
+    (10, 200),
+    (12312312313, 200)
+])
+def test_parametrize_delete_requests(base_url, post_id, status_code):
+    response = requests.delete(f"{base_url}/{post_id}")
+    assert response.status_code == status_code
